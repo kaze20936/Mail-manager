af77e7e05e33893e23ab20c6dedb0a4ff121b7ff
tomo <tomohiro8324y@gmail.com>
2026-04-18 15:53:27 +0900

    auto: app.py を更新にゃ

--- app.py
+++ app.py
@@ -1247,7 +1247,7 @@ def render_account_select_screen():
 
     # 別アカウントを追加するセクションにゃ
     with st.expander('➕ 別のアカウントを追加する'):
-        add_tab_g, add_tab_i = st.tabs(['🔴 Gmail (OAuth)', '📧 その他のメール (IMAP)'])
+        add_tab_g, add_tab_i = st.tabs(['🔴 Gmail', '📧 その他のメール (OCN・Outlook・Yahoo など)'])
         with add_tab_g:
             render_add_account_section()
         with add_tab_i:
